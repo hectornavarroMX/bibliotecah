3256db7d36c95e82f5e881a1c0b30c050d5aec73
hectorNavarroMX <52169961+rtsistemaspuebla@users.noreply.github.com>
2022-01-27 00:40:30 -0600

    se agregó listar

---
+++ app/Views/libros/listar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document Title</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+	Lista de libros
+	<div class="container">
+		<table class="table table-dark">
+			<thead class="thead-light">
+				<tr>
+					<th>#</th>
+					<th>Imágen</th>
+					<th>Nombre del libro</th>
+					<th>Acciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>ID</td>
+					<td>Imagen</td>
+					<td>Nombre</td>
+					<td>Editar/Borrar</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
